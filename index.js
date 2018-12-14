6186d04cbb3258f5698dfc656c9b4f3b185802fe
Francesco Longo <longo@ismb.it>
2018-12-14 11:26:12 +0100

    added different url and middleware

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/hello', (req, res) => hello_API(req, res));
+
+app.get('/bye', (req, res) => bye_API(req, res));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+let hello_API = function (req, res) {
+    res.send('Hello World!');
+};
+
+let bye_API = function (req, res) {
+    res.send('Bye Bye!');
+};
+
+app.use(function (req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+});
